5abdb3ba070764a28d37b3a007187ce2253be863
DrChainsaw <Christian.kyril.skarby@gmail.com>
2018-05-20 21:11:54 +0200

    Added workspace manager

--- src/main/java/ampcontrol/model/training/model/vertex/ChannelMultVertexImpl.java
+++ src/main/java/ampcontrol/model/training/model/vertex/ChannelMultVertexImpl.java
@@ -7,9 +7,9 @@ import org.deeplearning4j.nn.gradient.Gradient;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.graph.vertex.BaseGraphVertex;
 import org.deeplearning4j.nn.graph.vertex.VertexIndices;
-import org.nd4j.linalg.api.memory.MemoryWorkspace;
+import org.deeplearning4j.nn.workspace.ArrayType;
+import org.deeplearning4j.nn.workspace.LayerWorkspaceMgr;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.primitives.Pair;
 
 /**
@@ -21,12 +21,12 @@ import org.nd4j.linalg.primitives.Pair;
  */
 public class ChannelMultVertexImpl extends BaseGraphVertex {
 
-    ChannelMultVertexImpl(ComputationGraph graph, String name, int vertexIndex) {
+    public ChannelMultVertexImpl(ComputationGraph graph, String name, int vertexIndex) {
         this(graph, name, vertexIndex, null, null);
 
     }
 
-    ChannelMultVertexImpl(ComputationGraph graph, String name, int vertexIndex, VertexIndices[] inputVertices,
+    public ChannelMultVertexImpl(ComputationGraph graph, String name, int vertexIndex, VertexIndices[] inputVertices,
                                  VertexIndices[] outputVertices) {
         super(graph, name, vertexIndex, inputVertices, outputVertices);
     }
@@ -47,7 +47,7 @@ public class ChannelMultVertexImpl extends BaseGraphVertex {
     }
 
     @Override
-    public INDArray doForward(boolean training) {
+    public INDArray doForward(boolean training, LayerWorkspaceMgr workspaceMgr) {
         if (!canDoForward())
             throw new IllegalStateException("Cannot do forward pass: inputs not set " + this.toString());
 
@@ -55,7 +55,7 @@ public class ChannelMultVertexImpl extends BaseGraphVertex {
             throw new IllegalArgumentException(
                     this.toString() + " only supports 2 inputs.");
 
-        INDArray prod = inputs[0].dup(inputs[0].ordering());
+        INDArray prod = workspaceMgr.dup(ArrayType.ACTIVATIONS, inputs[0]);
         INDArray perCh = inputs[1];
 
         if (perCh.rank() != 2) {
@@ -87,40 +87,37 @@ public class ChannelMultVertexImpl extends BaseGraphVertex {
 
         // Goal: multiply each h*w activation a_i with the corresponding scale factor s_i, i in range [0, b*c-1]
 
+        final long nrofChannelBatch = channelActivations.tensorssAlongDimension(2, 3); // view each channel and each batch
+        final long nrofFeaturesInActivation = channelActivations.tensorssAlongDimension(0, 1); // view each h and w activation for all batches
 
-        final int nrofChannelBatch = channelActivations.tensorssAlongDimension(2, 3); // view each channel and each batch
-        final int nrofFeaturesInActivation = channelActivations.tensorssAlongDimension(0, 1); // view each h and w activation for all batches
-
-        try (MemoryWorkspace wsB = Nd4j.getWorkspaceManager()
-                .getWorkspaceForCurrentThread(ComputationGraph.workspaceExternal).notifyScopeBorrowed()) {
-            // From empiric testing: Whatever makes the fewest number of loops is fastest
-            if (nrofChannelBatch < nrofFeaturesInActivation) {
-                //  double time1 = System.nanoTime();
-                // Alt1: Loop over each channel and batch and multiply the resulting h*w with each scalar s
-                for (int i = 0; i < nrofChannelBatch; i++) {
-                    INDArray tens = channelActivations.tensorAlongDimension(i, 2, 3);
-                    tens.muli(scaleFactorsPerChannel.getDouble(i));
-                }
-                // double  time2 = System.nanoTime();
-                // System.out.println("loop scal mult: " + (time2 - time1) / 1e6);
-            } else {
-                // double time1 = System.nanoTime();
-                // Alt2: Loop over all h*w individual elements in the activation (shape [b,c]) and do elem wise multiplication
-                // Multiply each individual channel activation for all batches and channels
-                for (int i = 0; i < nrofFeaturesInActivation; i++) {
-                    INDArray tens = channelActivations.tensorAlongDimension(i, 0, 1);
-                    tens.muli(scaleFactorsPerChannel);
-                }
-                //  double time2 = System.nanoTime();
-                // System.out.println("loop vec mult: " + (time2 - time1) / 1e6);
+        // From empiric testing: Whatever makes the fewest number of loops is fastest
+        if (nrofChannelBatch < nrofFeaturesInActivation) {
+            //  double time1 = System.nanoTime();
+            // Alt1: Loop over each channel and batch and multiply the resulting h*w with each scalar s
+            for (int i = 0; i < nrofChannelBatch; i++) {
+                INDArray tens = channelActivations.tensorAlongDimension(i, 2, 3);
+                tens.muli(scaleFactorsPerChannel.getDouble(i));
             }
+            // double  time2 = System.nanoTime();
+            // System.out.println("loop scal mult: " + (time2 - time1) / 1e6);
+        } else {
+            // double time1 = System.nanoTime();
+            // Alt2: Loop over all h*w individual elements in the activation (shape [b,c]) and do elem wise multiplication
+            // Multiply each individual channel activation for all batches and channels
+            for (int i = 0; i < nrofFeaturesInActivation; i++) {
+                INDArray tens = channelActivations.tensorAlongDimension(i, 0, 1);
+                tens.muli(scaleFactorsPerChannel);
+            }
+            //  double time2 = System.nanoTime();
+            // System.out.println("loop vec mult: " + (time2 - time1) / 1e6);
         }
+
 
         return channelActivations;
     }
 
     @Override
-    public Pair<Gradient, INDArray[]> doBackward(boolean tbptt) {
+    public Pair<Gradient, INDArray[]> doBackward(boolean tbptt, LayerWorkspaceMgr workspaceMgr) {
         if (!canDoBackward())
             throw new IllegalStateException("Cannot do backward pass: errors not set " + this.toString());
 
@@ -136,27 +133,27 @@ public class ChannelMultVertexImpl extends BaseGraphVertex {
         // dL / du = epsilon * s // Same op as forward
         // dL / ds = epsilon * u // How to make this a scalar value??
 
-        INDArray eps_scale = epsilon.dup().muli(inputs[0]);
+        INDArray eps_scale = workspaceMgr.dup(ArrayType.ACTIVATION_GRAD, epsilon).muli(inputs[0]);
         // Error for each scale factor is sum of errors. Can't find a rational reason why though except other ways
         // either don't work dimensionally or just take me to NaNaNaNaN-land.
         // From my attempts to decipher paper authors caffe code this is what they are doing too
         // https://github.com/hujie-frank/SENet/blob/master/src/caffe/layers/axpy_layer.cpp
         INDArray out_scale = eps_scale.sum(2, 3);
+        out_scale = workspaceMgr.leverageTo(ArrayType.ACTIVATION_GRAD, out_scale);
 
         // Take a step of size mean(abs(eps_scale)) in the "dominant" direction of the error -> NaNaNaNaN-land
         //INDArray sum = eps_scale.sum(2,3);
         //INDArray sign = sum.div(abs(sum));
         //INDArray backSe = eps_scale.amean(2, 3).muli(sign);
 
-        INDArray out_conv = scaleChannels(epsilon.dup(), inputs[1]);
-
+        INDArray out_conv = scaleChannels(workspaceMgr.dup(ArrayType.ACTIVATION_GRAD, epsilon), inputs[1]);
         return new Pair<>(null, new INDArray[]{out_conv, out_scale});
     }
 
     @Override
     public void setBackpropGradientsViewArray(INDArray backpropGradientsViewArray) {
         if (backpropGradientsViewArray != null)
-            throw new IllegalArgumentException(
+            throw new RuntimeException(
                     "Vertex does not have gradients; gradients view array cannot be set here " + this.toString());
     }
 
