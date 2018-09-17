119e33a528d9175ece8845faf69b7b17db682b3f
DrChainsaw <Christian.kyril.skarby@gmail.com>
2018-09-18 00:01:22 +0200

    Better handling of multiple inputs

--- src/main/java/ampcontrol/model/training/model/evolve/mutate/MutateLayerContained.java
+++ src/main/java/ampcontrol/model/training/model/evolve/mutate/MutateLayerContained.java
@@ -9,11 +9,11 @@ import org.deeplearning4j.nn.graph.vertex.GraphVertex;
 import org.deeplearning4j.nn.graph.vertex.VertexIndices;
 import org.deeplearning4j.nn.transferlearning.TransferLearning;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -58,9 +58,11 @@ public class MutateLayerContained implements Mutation {
     private void replaceOrAddVertex(LayerMutation mutation, TransferLearning.GraphBuilder builder, ComputationGraph prevGraph) {
 
         if(Optional.ofNullable(prevGraph.getVertex(mutation.getLayerName())).isPresent())  {
+            // Simple case, we can replace an existing vertex
             builder.removeVertexKeepConnections(mutation.getLayerName());
         } else {
-            // layerN -> layerN+1 need to become layerN -> newlayer -> layerN+1
+            // Trickier case: Insert a vertex between two other vertices
+            // vertexN -> vertexN+1 need to become vertexN -> newlayer -> vertexN+1
             // Thus, layerN+1 must be removed and added back with newLayer as input
             for(String inputName: mutation.getInputLayers()) {
                 for(VertexIndices vertexIndices: prevGraph.getVertex(inputName).getOutputVertices()) {
@@ -70,12 +72,13 @@ public class MutateLayerContained implements Mutation {
                     final org.deeplearning4j.nn.conf.graph.GraphVertex vertexConf = prevGraph.getConfiguration().getVertices().get(outputName);
 
                     // Handle case of multiple inputs, like this:
-                    // {layerA, layerB} -> layerC shall become layerA -> newlayer and then {newlayer,layerB}-> layerC
-                    // Must add back layerB as input to layerC (outputVertex is layerC here).
-                    final List<String> allInputsToOutput = prevGraph.getConfiguration().getVertexInputs().get(outputName).stream()
-                            .filter(layername -> !inputName.equals(layername)) //inputName is the one we want to replace with mutation.layerName;
-                            .collect(Collectors.toList());
-                    allInputsToOutput.add(mutation.layerName);
+                    // {vertex0,..., vertexN} -> outputVertex shall become vertexi -> newlayer and then {vertex0,...,newlayer,...,vertexN}-> outputVertex
+                    // Must add back all other vertices as input to outputVertex.
+                    final List<String> allInputsToOutput = new ArrayList<>(prevGraph.getConfiguration().getVertexInputs().get(outputName));
+                    // Better insert new layer in the same place as the one which is removed...
+                    final int index = allInputsToOutput.indexOf(inputName);
+                    allInputsToOutput.remove(index);
+                    allInputsToOutput.add(index, mutation.layerName);
 
                     builder.removeVertexKeepConnections(outputVertex.getVertexName())
                             .addVertex(outputName,
