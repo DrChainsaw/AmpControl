238badbf98396662b1fc7c0ff05b9c32c66d143a
DrChainsaw <Christian.kyril.skarby@gmail.com>
2018-09-19 23:35:24 +0200

    Added PersistentSet.java

---
+++ src/main/java/ampcontrol/model/training/model/evolve/mutate/PersistentSet.java
@@ -0,0 +1,52 @@
+package ampcontrol.model.training.model.evolve.mutate;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * A set which may persist in a file.
+ *
+ * @param <T>
+ * @author Christian Skärby
+ */
+public class PersistentSet<T> {
+
+    private final File file;
+    private final Set<T> set;
+
+    public PersistentSet(
+            String fileName,
+            Collection<T> set) throws IOException {
+        this.file = new File(fileName);
+        this.set = new LinkedHashSet<>(set);
+        if (file.exists()) {
+            TypeReference ref = new TypeReference<Set<T>>() {};
+            this.set.addAll(new ObjectMapper().readValue(file, ref));
+        }
+    }
+
+    /**
+     * Returns the {@link Set}. Any changes made are backed by the set and may be persisted.
+     *
+     * @return the {@link Set}
+     */
+    public Set<T> get() {
+        return set;
+    }
+
+    /**
+     * Persist the set in a file
+     *
+     * @throws IOException
+     */
+    public void save() throws IOException {
+        file.getParentFile().mkdirs();
+        new ObjectMapper().writeValue(file, set);
+    }
+}
---
+++ src/test/java/ampcontrol/model/training/model/evolve/mutate/PersistentSetTest.java
@@ -0,0 +1,43 @@
+package ampcontrol.model.training.model.evolve.mutate;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Test cases for {@link PersistentSet}
+ */
+public class PersistentSetTest {
+
+    /**
+     * Test persisting a set and then loading it
+     */
+    @Test
+    public void persist() {
+        final Path baseDir = Paths.get("src", "test", "resources", "PersistentSetTest");
+        final String fileName = Paths.get(baseDir.toString(), "get.json").toString();
+
+        try {
+            final PersistentSet<String> expected = new PersistentSet<>(fileName, Stream.of("a", "test", "set").collect(Collectors.toSet()));
+            assertFalse("Set shall not be empty!", expected.get().isEmpty());
+            expected.save();
+            final PersistentSet<String> actual = new PersistentSet<>(fileName, Collections.emptySet());
+            assertEquals("Set did not persist!", expected.get(), actual.get());
+            new File(fileName).deleteOnExit();
+            new File(baseDir.toString()).deleteOnExit();
+        } catch (IOException e) {
+            throw new IllegalStateException();
+        }
+
+    }
+
+}
